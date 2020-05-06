4fb4951955d8a729183f0b9026e922317b9ed6c3
zhujiancom <eric87com@gmail.com>
2020-05-06 18:41:13 +0800

    fix(common): 字典项annotation解析
    支持多层级字典项annotation解析

--- src/main/java/com/ta/platform/common/tool/DictHelper.java
+++ src/main/java/com/ta/platform/common/tool/DictHelper.java
@@ -1,8 +1,12 @@
 package com.ta.platform.common.tool;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.ey.tax.toolset.core.ClassUtil;
 import com.ey.tax.toolset.core.ReflectUtil;
 import com.ey.tax.toolset.core.StrUtil;
+import com.ey.tax.toolset.core.TypeUtil;
+import com.ey.tax.toolset.core.collection.CollectionUtil;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -51,13 +56,26 @@ public class DictHelper {
                 log.info(" __翻译字典字段__ " + field.getName() + "_dictText： " + textValue);
                 item.put(field.getName() + "_dictText", textValue);
             }
-            //date类型默认转换string格式化日期
-            if (field.getType().getName().equals("java.util.Date") && field.getAnnotation(JsonFormat.class) == null && item.get(field.getName()) != null) {
-                SimpleDateFormat aDate = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-                item.put(field.getName(), aDate.format(new Date((Long) item.get(field.getName()))));
-            }
-            if (dictSupport(field.getType())){
-                item.put(field.getName(),parseDictField(ReflectUtil.getFieldValue(object, field)));
+            try {
+                //date类型默认转换string格式化日期
+                if (field.getType().getName().equals("java.util.Date") && field.getAnnotation(JsonFormat.class) == null && item.get(field.getName()) != null) {
+                    SimpleDateFormat aDate = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+                    item.put(field.getName(), aDate.format(new Date((Long) item.get(field.getName()))));
+                } else if (dictSupport(field.getType())) {
+                    item.put(field.getName(), parseDictField(ReflectUtil.getFieldValue(object, field)));
+                } else if (TypeUtil.getTypeArgument(field.getGenericType()) != null && dictSupport(Class.forName(TypeUtil.getTypeArgument(field.getGenericType()).getTypeName()))) {
+                    Object obj = ReflectUtil.getFieldValue(object, field);
+                    if(obj != null && Collection.class.isAssignableFrom(obj.getClass())){
+                        JSONArray jsonArray = new JSONArray();
+                        ((Collection)obj).stream().forEach(o->{
+                            JSONObject jso = parseDictField(o);
+                            jsonArray.add(jso);
+                        });
+                        item.put(field.getName(), jsonArray);
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                log.warn("字典项值转换出错！", e);
             }
         }
         return item;
